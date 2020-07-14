636842ea056e52a661838dad97ed4d5c47ad911d
mayor-b <betro1@gmail.com>
2020-07-13 17:33:12 -0700

    adding files from wednesday last week

--- SPA-In-Class/src/App/Nav.jsx
+++ SPA-In-Class/src/App/Nav.jsx
@@ -5,7 +5,10 @@ const Nav = () => {
     return (
         <NavStyled>
             <div className="nested-wrapper">
-                My navigation
+                <a href="/">Welcome</a>
+                <a href="/services">Services</a>
+                <a href="/contact">Contact</a>
+                <a href="/login">Login</a>
             </div>
         </NavStyled>
     );
@@ -16,4 +19,24 @@ export default Nav;
 const NavStyled = styled.nav`
     /* background-color: #dde7ea; */
     background-color: teal;
+    color: #fff;
+    text-align: center;
+
+    a {
+        display: inline-block;
+        font-size: 20px;
+        text-align: center;
+        line-height: 50px;
+        background-color: #034d4d;
+        text-decoration: none;
+        color: #fff;
+        width: 150px;
+        margin-right: 10px;
+        margin-left: 10px;
+        border-radius: 5px;
+
+        &:hover {
+            background-color: #000;
+        }
+    }
 `;
